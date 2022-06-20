15dc5f0328cc4284396a971f5e3b56fcc48892bb
Ryfless <89929482+Ryfless@users.noreply.github.com>
2022-06-20 23:37:29 +0700

    -

--- Getting Started/Declaration.cpp
+++ Getting Started/Declaration.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int main() {
-  int a;
+  int a; // <-- tidak perlu menambahkan nilai
   cin >> a; // <-- Deklarasi
-  cout << a << endl;
+  cout << a << endl; // <-- Menampilkan
   
   return 0;
 }
+
+/* Deklarasi adalah fungsi untuk memasukan suatu nilai pada variabel */
