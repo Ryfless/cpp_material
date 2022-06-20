a04d98964eddcbb2af9449fd00c7b0fee0f4bd28
Ryfless <89929482+Ryfless@users.noreply.github.com>
2022-06-20 22:39:34 +0700

    gettingStarted.cpp

---
+++ gettingStarted.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // <--- Ini adalah Standard Library
+
+using namespace std; // <--- namespace
+
+int main() {
+  cout << "Hello World!" << endl; // <--- fungsi utama
+
+  return 0;
+}
