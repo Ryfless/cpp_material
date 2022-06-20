1a8c04e8bf8cd41c4e0b8726817b3f8d68991822
Ryfless <89929482+Ryfless@users.noreply.github.com>
2022-06-20 23:47:32 +0700

    -

---
+++ Getting Started/Operator_aritmatika.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b, hasil;
+  a = 2;
+  b = 3;
+  hasil = a + b; /* <-- Operasi aritmatika dapat menggunakan +, -, /, *, % 
+                        + untuk Menambah
+                        - untuk Mengurangi
+                        / untuk Membagi
+                        * untuk Mengali
+                        % untuk Modulo (sisa bagi)
+                */
+  cout << hasil << endl;
+  
+  return 0;
+}
+
+/* Output = 5 */
